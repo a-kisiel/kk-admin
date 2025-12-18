8886935d6ef8042d9cd1050fa2a4370ff8a17d75
Alex Kisiel <arkisiel01@gmail.com>
2025-12-18 02:08:30 -0500

    Package updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { NavComponent } from './components/nav/nav.component';
 import { RouterOutlet } from '@angular/router';
+import 'zone.js'
 
 @Component({
   selector: 'app-root',
